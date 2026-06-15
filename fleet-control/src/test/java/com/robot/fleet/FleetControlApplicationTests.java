2d4f3d5e830b1882692e34130ed48684be0d67e5
indsf <northfce@naver.com>
2026-06-15 13:43:55 +0900

    add

---
+++ fleet-control/src/test/java/com/robot/fleet/FleetControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.fleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
